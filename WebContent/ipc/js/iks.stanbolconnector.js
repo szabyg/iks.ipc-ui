f741037bcd506815df1a0817a3c815d1b7814b2c
Szaby Grünwald <szaby.gruenwald@salzburgresearch.at>
2011-03-05 21:12:56 +0100

    Implement connection to local stanbol and do autocomplete based on the srfg project, person and task data.

--- WebContent/ipc/js/iks.stanbolconnector.js
+++ WebContent/ipc/js/iks.stanbolconnector.js
@@ -1,50 +1,94 @@
 (function($){
 $.stanbolConnector = {
     options: {
-        stanbolUrl: "localhost:8080/",
+        stanbolUrl: "http://localhost:8080/",
         proxyUrl: "proxy/proxy.php",
         error: function(errMsg){alert(errMsg);}
     },
     /**
      * Get the entityhub sitelist
+     * @cb callback, gets an array of site names, e.g. ["dbpedia", "geonames"]
      */
     getSites: function(cb, options){
         $.extend(this.options, options);
-        console.info("ajax...");
-        
         var uri = this.options.stanbolUrl + "entityhub/sites/referenced";
         var that = this;
-        console.info(uri);
-        var xhr = $.ajax(
-            {
-                url: this.options.proxyUrl, 
-                type: "POST",
-                data: {
-                    proxy_url: uri, 
-//                    format: "application/rdf+json", 
-                    verb: "GET"
-                },
-                success: function (data, textStatus, jqXHR){
-                    _.map([1, 2, 3], function(num){ return num * 3; });
-                    var res = 
-                    _.map(data, function(siteUri){
-                        return siteUri.replace(this.options.stanbolUrl + "entityhub/site/","").replace("/","")
-                    }, that);
-                    cb(res);
-                },
-                error: function(jqXHR, textStatus, errorThrown){
-                    console.info(textStatus);
-                },
+        this.stanbolRequest(uri,{
+            acceptHeader: "application/json", 
+            success: function (data, textStatus, jqXHR){
+                var res = _.map(data, function(siteUri){
+                    return siteUri.replace(this.options.stanbolUrl + "entityhub/site/","").replace("/","")
+                }, that);
+                cb(res);
             }
-        );
-        console.info(xhr);
+        });
     },
-    stanbolAlive: function(options){
-        return true;
+    entityhubQuery: function(site, s, cb, options){
+        var uri = this.options.stanbolUrl + "entityhub/site/" + site + "/query";
+        var fieldQuery = {
+            "selected": [ 
+                "http:\/\/www.w3.org\/2000\/01\/rdf-schema#label", 
+                "http:\/\/www.w3.org\/1999\/02\/22-rdf-syntax-ns#type"], 
+            "offset": "0", 
+            "limit": "11", 
+            "constraints": [{ 
+               "type": "text", 
+               "patternType": "wildcard", 
+               "text": "*" + s + "*", 
+               "field": "http:\/\/www.w3.org\/2000\/01\/rdf-schema#label" 
+            }]
+        };
+        var postData = {query: JSON.serialize(fieldQuery)};
+        this.stanbolRequest(uri,{
+            method: "POST",
+            success: function (data, textStatus, jqXHR){
+                cb(_.map(data.results, function(entity){
+                    var res = {};
+                    _.each(entity, function(value, key){
+                        if(_.isArray(value)){
+                            res[key] = _.map(value, function(valueObj){
+                                return valueObj.value;
+                            });
+                        } else {
+                            res[key] = value;
+                        }
+                    });
+                    return res;
+                }));
+            },
+            data: postData,
+            processData: false,
+            dataType: "string"
+        });
+    },
+    getEntity: function(site, uri, cb, options){
+    
+    },
+    /**
+     * generic call to a stanbol backend through a proxy
+     * 
+     */
+    stanbolRequest: function(uri, options){
+        var ajaxOpt = 
+        {
+            url: this.options.proxyUrl, 
+            type: "POST",
+            data: {
+                proxy_url: uri, 
+                Accept: options.acceptHeader || "application/json", 
+                verb: options.method || "GET"
+            },
+            success: options.success,
+            error: options.error
+        };
+        $.extend(ajaxOpt.data, options.data);
+        $.ajax(ajaxOpt);
+        
     },
     autocompleteFn: function(sites, options){
+        
+    },
     
-    }
     
 }
 })(jQuery)
